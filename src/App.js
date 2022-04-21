12a2a50d362cd03454a2815c1df4bf72cb74c1f8
Marcin_Cer <midnajt0@gmail.com>
2022-04-21 17:03:09 +0200

    Navbar.js created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar.js';
+import Home from './pages/Home.js';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            {/* <Button variant="contained" color="primary">
+                Hello World
+            </Button> */}
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" exact element={<Home/>} />
+        </Routes>
+        {/* <Footer/> */}
+      </Router>
     </div>
   );
 }
