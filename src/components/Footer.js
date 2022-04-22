acb2523e38618e1553d12a35974e55c941b54087
Marcin_Cer <midnajt0@gmail.com>
2022-04-22 16:04:44 +0200

    Footer.js added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import '../styles/Footer.css';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+        <div className='socialMedia'>
+            <div className='socialMedia'>
+                <InstagramIcon/>
+                <GitHubIcon/>
+                <FacebookIcon/>
+                <LinkedInIcon/>
+            </div>
+            <p>&copy; 2022pedrotechpizza.com</p>
+        </div>
+    </div>
   )
 }
