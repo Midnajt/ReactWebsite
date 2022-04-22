a9ed8a1b32e2f8ecb34124d403ebae6598ad2133
Marcin_Cer <midnajt0@gmail.com>
2022-04-22 15:46:38 +0200

    Home.js created

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar.js';
+import Footer from './components/Footer.js';
 import Home from './pages/Home.js';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-            {/* <Button variant="contained" color="primary">
-                Hello World
-            </Button> */}
       <Router>
         <Navbar/>
         <Routes>
           <Route path="/" exact element={<Home/>} />
         </Routes>
-        {/* <Footer/> */}
+        <Footer/>
       </Router>
     </div>
   );
